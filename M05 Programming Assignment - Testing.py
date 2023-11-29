1a778c88e812fd5e79e8aa8212364b6afab4bfbe
bkarima <149276785+bkarima@users.noreply.github.com>
2023-11-28 21:07:39 -0500

    Add files via upload

---
+++ M05 Programming Assignment - Testing.py
@@ -0,0 +1,46 @@
+#test_sum_unittest.py
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
+ #test_sum.py
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
+#test.py
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
